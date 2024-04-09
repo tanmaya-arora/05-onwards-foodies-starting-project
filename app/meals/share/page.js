33b1a8acae08e63d27e63b783360600c206a6b49
tanmaya-arora <tanmaya.arora@ikokas.com>
2024-04-09 11:28:50 +0530

    Initial commit

---
+++ app/meals/share/page.js
@@ -0,0 +1,10 @@
+export default function MealsShare() {
+    return (
+      <main>
+        <h1 style={{ color: 'white', textAlign: 'center' }}>
+          Meals share page!
+        </h1>
+      </main>
+    );
+  }
+  
---
+++ components/main-header/main-header.js
@@ -0,0 +1,33 @@
+'use client';
+
+import Link from "next/link";
+import logoImg from '@/assets/logo.png';
+import classes from "./main-header.module.css";
+import { usePathname } from "next/navigation";
+
+export default function MainHeader() {
+    const path = usePathname();
+    
+    return <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+            <img src={logoImg.src} alt="A plate with food on it" priority />
+            NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+            <ul>
+                <li>
+                    <Link href="/meals"
+                    className={
+                        path.startsWith("/meals")? classes.active : undefined
+                    }>Browse Meals </Link >
+                </li>
+                <li>
+                    <Link href="/community" className={
+                        path === "/community" ? classes.active : undefined
+                    }>Foodies Community </Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+}
