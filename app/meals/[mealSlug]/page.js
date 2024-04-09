40411e6b6977d4cd09795c577746d10bb0c14235
tanmaya-arora <tanmaya.arora@ikokas.com>
2024-04-09 15:47:32 +0530

    Added Meal item page to view details of a specific meal by slug

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,27 @@
-export default function MealDetailsPage() {
-    return <h1>Meal Details</h1>
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
+
+export default function MealDetailsPage({params}) {
+    const meal = getMeal(params.mealSlug);
+
+    return (
+        <>
+        <header className={classes.header}>
+            <div className={classes.image}>
+                <Image src={meal.image} alt={meal.title} layout="fill" objectFit="cover" />
+            </div>
+            <div className={classes.headerText}>
+                <h1>{meal.title}</h1>
+                <p className={classes.creator}>
+                    by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                </p>
+                <p className={classes.summary}>{meal.summary}</p>
+            </div>
+        </header>
+        <main>
+            <p className={classes.instructions}>{meal.instructions}</p>
+        </main>
+        </>
+    )
 }
