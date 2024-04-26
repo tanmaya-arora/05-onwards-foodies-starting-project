c583771443c739ca8b84a9d24004566f239e3bed
tanmaya-arora <tanmaya.arora@ikokas.com>
2024-04-26 15:05:23 +0530

    Saving meal functionality

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,6 +2,21 @@ import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
+  async function shareMeal(formData) {
+    'use server';
+
+    const meal = {
+      title: formData.get('title'),
+      summary: formData.get('summary'),
+      instructions: formData.get('instructions'),
+      image: formData.get('image'),
+      creator: formData.get('name'),
+      email: formData.get('email')
+    }
+
+    console.log("Meal saved -- ",meal)
+  }
+
   return (
     <>
       <header className={classes.header}>
--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -45,6 +45,7 @@ export default function ImagePicker({ label, name }) {
                 name={name}
                 ref={imageInput}
                 onChange={handleImageChange}
+                required
                 />
                 <button className={classes.button} type="button" onClick={handlePickClick}>
                     Pick an image
